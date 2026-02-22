33d0478db0279fb5b26c3d33ccfce8dac333304b
petersonljr <58838237+petersonljr@users.noreply.github.com>
2026-02-22 17:03:12 -0300

    Add files via upload

---
+++ Medico.java
@@ -0,0 +1,8 @@
+package com.fatec.ads.poo;
+
+public class Medico {
+    private Long id;
+    private String nome;
+    private String crm;
+    private String especialidade;
+}
---
+++ Paciente.java
@@ -0,0 +1,9 @@
+package com.fatec.ads.poo;
+
+public class Paciente {
+    private String nome;
+	private int idade;
+	private String endereco;
+    private double altura;
+    private double peso;
+}
